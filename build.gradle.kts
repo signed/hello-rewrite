82f9c53f33c2c6c2e8e70a5d701c72eb2d0c1b0c
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-10-12 16:33:56 +0200

    publish to local repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,13 @@
 plugins {
+    `maven-publish`
     id("org.openrewrite.build.recipe-library-base") version "latest.release"
-
-    // This uses the nexus publishing plugin to publish to the moderne-dev repository
-    // Remove it if you prefer to publish by other means, such as the maven-publish plugin
-    id("org.openrewrite.build.publish") version "latest.release"
-    id("nebula.release") version "latest.release"
-
-    // Configures artifact repositories used for dependency resolution to include maven central and nexus snapshots.
-    // If you are operating in an environment where public repositories are not accessible, we recommend using a
-    // virtual repository which mirrors both maven central and nexus snapshots.
-    id("org.openrewrite.build.recipe-repositories") version "latest.release"
-
     // Only needed when you want to apply the OpenRewriteBestPractices recipe to your recipes through
     // ./gradlew rewriteRun -Drewrite.activeRecipe=org.openrewrite.recipes.OpenRewriteBestPractices
     id("org.openrewrite.rewrite") version "latest.release"
+}
+
+repositories {
+    mavenCentral()
 }
 
 // Set as appropriate for your organization
@@ -53,29 +47,25 @@ dependencies {
     rewrite("org.openrewrite.recipe:rewrite-recommendations:latest.release")
 }
 
-signing {
-    // To enable signing have your CI workflow set the "signingKey" and "signingPassword" Gradle project properties
-    isRequired = false
-}
-
-// Use maven-style "SNAPSHOT" versioning for non-release builds
-configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
-    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
-configure<PublishingExtension> {
-    publications {
-        named("nebula", MavenPublication::class.java) {
-            suppressPomMetadataWarningsFor("runtimeElements")
-        }
-    }
-}
 
 publishing {
-  repositories {
-      maven {
-          name = "moderne"
-          url = uri("https://us-west1-maven.pkg.dev/moderne-dev/moderne-recipe")
-      }
-  }
+    repositories {
+        maven {
+            name = "local"
+            url = uri("http://localhost:8080/snapshots")
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+            isAllowInsecureProtocol = true
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.signed"
+            artifactId = "hello-rewrite"
+            version = "0.1.0-SNAPSHOT"
+            from(components["java"])
+        }
+    }
 }
