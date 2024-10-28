f3d22f002bcd01fc60e1a2a7bf605c2de729b6fc
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-10-28 23:43:33 +0100

    reproducer

--- src/test/java/reproducer/MakeStaticTest.java
+++ src/test/java/reproducer/MakeStaticTest.java
@@ -1,14 +1,18 @@
 package reproducer;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.Cursor;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.java.JavaTemplate;
+import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.UseStaticImport;
-import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.java.tree.J;
 import org.openrewrite.test.RewriteTest;
 import org.openrewrite.test.SourceSpecs;
 
-import java.util.function.Consumer;
-
 import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.test.RewriteTest.toRecipe;
 
 public class MakeStaticTest implements RewriteTest {
 
@@ -36,13 +40,11 @@ public class MakeStaticTest implements RewriteTest {
         }
         """
     );
+    private UseStaticImport makeFactoryImportStatic = new UseStaticImport("org.example.createviafactory.another.HelloFactory createHello(String, long)");
 
     @Test
     void name() {
-        final Consumer<RecipeSpec> spec = recipeSpec -> {
-            recipeSpec.recipe(new UseStaticImport("org.example.createviafactory.another.HelloFactory createHello(String, long)"));
-        };
-        rewriteRun(spec,
+        rewriteRun(recipeSpec -> recipeSpec.recipe(makeFactoryImportStatic),
           helloClass,
           helloFactoryClass,
           java(
@@ -55,6 +57,66 @@ public class MakeStaticTest implements RewriteTest {
               
                   public void example() {
                       HelloFactory.createHello("Alice", 42L);
+                  }
+              }
+              """, """
+              package org.example.createviafactory.usage;
+              
+              import static org.example.createviafactory.another.HelloFactory.createHello;
+              
+              public class ConstructorCall {
+              
+                  public void example() {
+                      createHello("Alice", 42L);
+                  }
+              }
+              """));
+    }
+
+    @Test
+    void noStaticImportAdded() {
+        rewriteRun(recipeSpec -> {
+              recipeSpec.recipe(toRecipe(() -> new JavaVisitor<>() {
+                  MethodMatcher FactoryMethod = new MethodMatcher("org.example.createviafactory.another.HelloFactory createHello(String, long)");
+                    @Override
+                    public J visitNewClass(J.NewClass newClass, ExecutionContext executionContext) {
+                        final var result = (J.NewClass) super.visitNewClass(newClass, executionContext);
+                        final Cursor parent = getCursor().getParentOrThrow();
+                        final J.MethodDeclaration methodDeclaration = parent.firstEnclosing(J.MethodDeclaration.class);
+                        final J.ClassDeclaration classDeclaration = parent.firstEnclosing(J.ClassDeclaration.class);
+                        final var isInstantiationInFactoryMethod = FactoryMethod.matches(methodDeclaration, classDeclaration);
+                        if (isInstantiationInFactoryMethod) {
+                            return result;
+                        }
+
+                        final var template = JavaTemplate.builder("HelloFactory.createHello(#{any()}, #{any()})")
+                          .doBeforeParseTemplate((code)-> System.out.println(code))
+                          .imports("org.example.createviafactory.another.HelloFactory")
+                          .build();
+                        maybeRemoveImport("org.example.createviafactory.Hello");
+                        // why does this one not find the usage and needs false?
+                        maybeAddImport("org.example.createviafactory.another.HelloFactory", false);
+                        // you are also having the cache problem?
+                        doAfterVisit(makeFactoryImportStatic.getVisitor());
+
+                        final var arguments = result.getArguments();
+                        return template.apply(getCursor(), result.getCoordinates().replace(), arguments.get(0), arguments.get(1));
+                    }
+                }
+              ));
+          },
+          helloClass,
+          helloFactoryClass,
+          java(
+            """
+              package org.example.createviafactory.usage;
+              
+              import org.example.createviafactory.Hello;
+              
+              public class ConstructorCall {
+              
+                  public void example() {
+                      new Hello("Alice", 42L);
                   }
               }
               """, """
